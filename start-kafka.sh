21ecd0e98f791d98b03de0cc183526812298576e
Huahai Yang <huahai.yang@gmail.com>
2014-11-08 23:17:37 -0800

    make borker.id configurable

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-export KAFKA_ADVERTISED_PORT=$(docker port `hostname` 9092 | sed -r "s/.*:(.*)/\1/g")
-export KAFKA_BROKER_ID=$KAFKA_ADVERTISED_PORT
-
-export KAFKA_LOG_DIRS="\/kafka\/kafka-logs-$KAFKA_BROKER_ID"
-
+if [[ -z "$KAFKA_ADVERTISED_PORT" ]]; then
+  export KAFKA_ADVERTISED_PORT=$(docker port `hostname` 9092 | sed -r "s/.*:(.*)/\1/g")
+fi
+if [[ -z "$KAFKA_BROKER_ID" ]]; then
+  export KAFKA_BROKER_ID=$KAFKA_ADVERTISED_PORT
+fi
+if [[ -z "$KAFKA_LOG_DIRS" ]]; then
+  export KAFKA_LOG_DIRS="/kafka/kafka-logs-$KAFKA_BROKER_ID"
+fi
 if [[ -z "$KAFKA_ZOOKEEPER_CONNECT" ]]; then
-    export KAFKA_ZOOKEEPER_CONNECT=$ZK_PORT_2181_TCP_ADDR
+  export KAFKA_ZOOKEEPER_CONNECT=$(env | grep ZK.*PORT_2181_TCP= | sed -e 's|.*tcp://||' | paste -sd ,)
 fi
 
 if [[ -n "$KAFKA_HEAP_OPTS" ]]; then
-    sed -r -i "s/^(export KAFKA_HEAP_OPTS)=\"(.*)\"/\1=\"$KAFKA_HEAP_OPTS\"/g" $KAFKA_HOME/bin/kafka-server-start.sh
-    unset KAFKA_HEAP_OPTS
+  sed -r -i "s/^(export KAFKA_HEAP_OPTS)=\"(.*)\"/\1=\"$KAFKA_HEAP_OPTS\"/g" $KAFKA_HOME/bin/kafka-server-start.sh
+  unset KAFKA_HEAP_OPTS
 fi
 
 for VAR in `env`
@@ -20,9 +24,9 @@ do
     kafka_name=`echo "$VAR" | sed -r "s/KAFKA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
     env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
     if egrep -q "(^|^#)$kafka_name" $KAFKA_HOME/config/server.properties; then
-        sed -r -i "s/(^|^#)($kafka_name)=(.*)/\2=${!env_var}/g" $KAFKA_HOME/config/server.properties
+      sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties 
     else
-        echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
+      echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
     fi
   fi
 done
